07fa9c450f13d0c81a8ba9422279512e00103b0a
IvanPartsunev <ivan.parcunev@gmail.com>
2024-09-28 14:30:35 +0300

    refactor: url patterns

--- polls_app/core/urls.py
+++ polls_app/core/urls.py
@@ -6,30 +6,10 @@ from polls_app.core.views import ProductsListCreateApiView, ProductRetrieveUpdat
 urlpatterns = (
     path("products/", ProductsListCreateApiView.as_view(), name="products"),
     path("products/<int:pk>/", ProductRetrieveUpdateDeleteApiView.as_view(), name="product"),
-    path("products/<int:product_pk>/questions", QuestionsListApiView.as_view(), name="questions"),
-    path("products/<int:product_pk>/questions/<int:question_pk>/", QuestionsApiView.as_view(), name="question"),
-    path(
-        "questions/<int:question_pk>/answers/",
-        AnswersCreateApiView.as_view(),
-        name="answer"
-    ),
-
-    path(
-        "questions/<int:question_pk>/answers/<int:answer_pk>",
-        AnswersReadUpdateDeleteApiView.as_view(),
-        name="answers"
-    ),
-
-    path(
-        "questions/<int:question_pk>/comments/",
-        CommentsCreateApiView.as_view(),
-        name="comment"
-    ),
-
-    path(
-        "questions/<int:question_pk>/comments/<int:comment_pk>/",
-        CommentsReadUpdateDeleteApiView.as_view(),
-        name="comments"
-    ),
-
+    path("questions/", QuestionsListApiView.as_view(), name="questions"),
+    path("questions/<int:question_pk>/", QuestionsApiView.as_view(), name="question"),
+    path("answers/", AnswersCreateApiView.as_view(), name="answer"),
+    path("answers/<int:answer_pk>", AnswersReadUpdateDeleteApiView.as_view(), name="answers"),
+    path("comments/", CommentsCreateApiView.as_view(), name="comment"),
+    path("comments/<int:comment_pk>/", CommentsReadUpdateDeleteApiView.as_view(), name="comments"),
 )

5219d9ffc0bb8da2194f93a9a8d49d7dcc755c54
IvanPartsunev <ivan.parcunev@gmail.com>
2024-10-20 16:57:23 +0300

    created JWT token creation and encode service for anonymous users

--- polls_app/core/services.py
+++ polls_app/core/services.py
@@ -1,8 +1,14 @@
+import jwt
+
+from datetime import timedelta
+
+from django.utils import timezone
 from django.http import Http404
 from django.contrib.contenttypes.models import ContentType
 
 from polls_app.core.permissions import is_owner
 from polls_app.custom_exeption import ApplicationError
+from polls_app.settings import SECRET_KEY
 
 
 def get_object_and_check_permission_service(app_label, model_name, obj_id, user):
@@ -22,3 +28,28 @@ def get_object_and_check_permission_service(app_label, model_name, obj_id, user)
         is_owner(current_object, user)
 
     return current_object
+
+
+class AnonymousUserJWTService:
+
+    @staticmethod
+    def generate_comment_jwt_token(guest_id):
+        payload = {
+            "guest_id": guest_id,
+            "exp": timezone.now() + timedelta(hours=24),
+            "scope": "comment",
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+        return token
+
+    @staticmethod
+    def decode_comment_jwt_token(token):
+        try:
+            payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+            if payload.get("scope") != "comment":
+                raise jwt.InvalidTokenError("Invalid scope for token")
+            return payload
+        except jwt.ExpiredSignatureError:
+            raise jwt.ExpiredSignatureError("Token has expired")
+        except jwt.InvalidTokenError:
+            raise jwt.InvalidTokenError("Invalid token")
